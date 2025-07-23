5b389a863d485faf105c6e4297e4b5eb8091d231
m-ball3 <mball3@uw.edu>
2025-07-23 10:54:42 -0700

    Updates write to excel

--- write to excel.R
+++ write to excel.R
@@ -6,7 +6,7 @@ class("taxa")
 
 df <- as.data.frame(taxa)
 df_with_rownames <- cbind(rownames = rownames(df), df)
-writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred-16SP2-taxa.xlsx")
+writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred-12SP1-taxa-130trunc.xlsx")
 
 
 write_xlsx(df, "mWADE003-arcticpred-16SP2-taxa.xlsx")
