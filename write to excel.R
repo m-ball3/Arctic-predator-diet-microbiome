80723c98332f01db0520e930a605715f4b50d7f0
m-ball3 <mball3@uw.edu>
2025-08-05 15:08:34 -0700

    Save my local changes

--- write to excel.R
+++ write to excel.R
@@ -6,7 +6,7 @@ class("taxa")
 
 df <- as.data.frame(taxa)
 df_with_rownames <- cbind(rownames = rownames(df), df)
-writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred-12SP1-taxa-130trunc2.xlsx")
+writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred-12SP1-taxa-130trunc7.xlsx")
 
 
 write_xlsx(df, "mWADE003-arcticpred-16SP2-taxa.xlsx")
