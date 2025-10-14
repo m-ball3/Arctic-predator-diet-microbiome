b7685099ba2a5c9d8bf78c29f1441f8880e1d487
Mollie Ball <mball3@uw.edu>
2025-10-13 17:56:11 -0700

    updates with left trim = 20

--- write to excel.R
+++ write to excel.R
@@ -6,7 +6,7 @@ class("taxa")
 
 df <- as.data.frame(taxa)
 df_with_rownames <- cbind(rownames = rownames(df), df)
-writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred_dada2_QAQC_16SP2_output-ADFGnotes.xlsx")
+writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred_dada2_QAQC_12SP1_output-130trunc-ADFGnotes-50BOOTTRUE-leftrim20.xlsx")
 
 
 write_xlsx(df, "mWADE003-arcticpred-16SP2-taxa.xlsx")
