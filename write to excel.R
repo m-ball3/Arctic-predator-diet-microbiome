49a6a35bb9c23a3c3ae43341137bbcfaf41e6224
m-ball3 <mball3@uw.edu>
2025-11-03 15:05:34 -0800

    updates write to excel

--- write to excel.R
+++ write to excel.R
@@ -4,9 +4,9 @@ library(writexl)
 ls()
 class("taxa")
 
-df <- as.data.frame(taxa)
+df <- as.data.frame(species)
 df_with_rownames <- cbind(rownames = rownames(df), df)
-writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred_dada2_QAQC_12SP1_output-130trunc-ADFGnotes-50BOOTTRUE-leftrim20.xlsx")
+writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred_dada2_QAQC_12SP1_output-addspecies.xlsx")
 
 
 write_xlsx(df, "mWADE003-arcticpred-16SP2-taxa.xlsx")
