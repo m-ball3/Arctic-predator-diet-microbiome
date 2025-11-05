d04f7d97d1225c6a112f7c83946ecd5ede99defd
m-ball3 <mball3@uw.edu>
2025-11-05 10:57:07 -0800

    updates with work on addpecies function

--- Scripts/write to excel.R
+++ Scripts/write to excel.R
@@ -6,7 +6,7 @@ class("taxa")
 
 df <- as.data.frame(species)
 df_with_rownames <- cbind(rownames = rownames(df), df)
-writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred_dada2_QAQC_12SP1_output-addspecies.xlsx")
+writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred_dada2_QAQC_12SP1_output-addspecies-3.xlsx")
 
 
 write_xlsx(df, "mWADE003-arcticpred-16SP2-taxa.xlsx")
