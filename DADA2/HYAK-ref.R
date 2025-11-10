eac5e61548ce0b672a63b7e63965a683753d14f7
Mollie Ball <mball3@uw.edu>
2025-11-10 11:01:49 -0800

    Updates with new addspecies AND assignspecies concatenated addtaxonomy table

--- DADA2/HYAK-ref.R
+++ DADA2/HYAK-ref.R
@@ -35,8 +35,11 @@ scp -r "C:/Users/MBall/OneDrive/文档/WADE LAB/Arctic Predator Diets/12SP1" mba
 scp -r C:/Users/Intern/Desktop/arctic-pred/scripts/ mball3@klone.hyak.uw.edu:/gscratch/coenv/mball3/WADE003-arctic-pred/scripts
   # MUST BE RUN FROM LOCAL TERMINAL! NOT HYAK SSH SESSION
 
+scp -r "mball3@klone.hyak.uw.edu:/mmfs1/home/mball3/WADE003-arcticpred_dada2_QAQC_12SP1_output-addSpecies-130;30-3.Rdata" "C:/Users/MBall/OneDrive/Documents/UW-DOCS/WADE lab/Arctic-predator-diet-microbiome/DADA2/DADA2 Outputs"
 
 scp -r "C:\Users\Intern\Desktop\arctic-pred\16S_Arctic_predator_reference_database_05_2025.fasta" mball3@klone.hyak.uw.edu:/gscratch/coenv/mball3/WADE003-arctic-pred/scripts
+
+
 
 # Read error output
 cat file
