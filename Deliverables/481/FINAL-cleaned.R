eab7a97a9e5f35955e9229bc03d27b8d9c0f767f
Mollie Ball <mball3@uw.edu>
2025-12-08 14:09:43 -0800

    updates with final projects for autumn 2025 quarter

--- Deliverables/481/FINAL-cleaned.R
+++ Deliverables/481/FINAL-cleaned.R
@@ -15,6 +15,9 @@ library(agricolae)   # for post-hoc tests and letters
 library(patchwork)   # for plot layout (if not already loaded)
 library(MuMIn)
 library(lmtest)
+library(ggeffects)
+library(png)
+library(grid)
 
 # ------------------------------------------------------------------
 # Loads in sample data
@@ -103,7 +106,8 @@ species_richness_long_all <- samdf_unique %>%
     TRUE ~ Marker
   ))
 
-
+# df that includes all sucessfully amplified ssamples in 12s and 16s 
+# (not just the ones that were successful for both 12s and 16s)
 species_richness_long_all <- species_richness_long_all %>%
   left_join(samdf_unique %>% 
               select(Specimen.ID, Location, Sex, Season, Location.in.body, Predator, Year),
@@ -118,7 +122,10 @@ hist(na.omit(species_richness_long_all$Year), main = NA, xlab = "Year", ylab= "N
 # Creates a df with just sames and whether they were male or female
 sex.df <- species_richness_long_all %>%
   filter(Sex %in% c("M", "F")) %>%
-  select(Specimen.ID, Species.Richness, Sex, Predator, Marker)
+  select(Specimen.ID, Species.Richness, Sex, Predator)%>%
+  mutate(Sex = as.factor(Sex))%>%
+  mutate(Predator = as.factor(Predator))
+
 
 # samdf_unique is filtered by sample ID's that passed QAQC
 nrow(sex.df) # = 61
@@ -172,9 +179,6 @@ pred.p | sex.p
 # ------------------------------------------------------------------
 # Fits Model
 # ------------------------------------------------------------------
-## when i add more variables, how does it change AOV (just predator vs just sex)
-
-
 # Models
 
 pred.lm <- lm(Species.Richness ~ Predator, sex.df, na.action = na.fail)
@@ -205,14 +209,14 @@ qqline(res)
 # Shapiro test for normality
 shapiro.test(res) #p-value = 0.1891
 
-# levene test for homogeneity of variance 3 NEED TO DO THIS FOR OTHER INDEPENDENT VARIABLES
+# levene test for homogeneity of variance
 leveneTest(pred.lm)
 
 # Checking for influenial outliers
 plot(pred.lm.log, which = 1)  # Residuals vs Fitted
 
 # Cook's distance
-plot(pred.lm, which = 4)  # Cook's distance plot
+plot(pred.lm, which = 4)
 
 # MODEL CHECKS
 dredge(pred.sex.int.log)
@@ -220,6 +224,38 @@ dredge(pred.sex.int.log)
 ??lrtest()
 lrtest(pred.lm.log, pred.sex.log, pred.sex.int.log)
 
-# Report AICS 
+# CHOSEN MODEL
+summary(pred.sex.log)
+plot(pred.sex.log)
 
+# creates predicted values
+
+pred <- ggpredict(pred.sex.log, terms = c("Predator", "Sex"))
+
+# Model fitness plot
+f.pred <- ggplot(pred,
+       aes(x = x, y = predicted, fill = group)) +
+  geom_col(position = "dodge", alpha = 0.8, width = 0.7) +
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high),
+                position = position_dodge(0.7), width = 0.25) +
+  geom_point(data = sex.df, 
+             aes(x = Predator, y = Species.Richness.log, color = Sex),
+             inherit.aes = FALSE,
+             position = position_jitterdodge(dodge.width = 0.7, jitter.width = 0.15), 
+             alpha = 0.7, size = 2) +
+  labs(title = "Predicted log(Species Richness) by Predator and Sex",
+       x = "Predator", y = "log(Species Richness)", 
+       fill = "Sex", color = "Sex") +
+  scale_fill_manual(values = c("F" = "#1f78b4", "M" = "lightblue")) +
+  scale_color_manual(values = c("F" = "#153e5e", "M" = "lightblue4")) +
+  theme_light() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "bottom",
+        panel.grid.minor = element_blank())
+f.pred
+
+# Add Lab ID to otu.abs
+write.csv(sex.df,
+          "./Deliverables/481/pred.sex.df.csv", 
+          row.names = FALSE)
 
