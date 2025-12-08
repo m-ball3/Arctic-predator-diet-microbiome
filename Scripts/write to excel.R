bbbd991fee6ecda78e214407df40d448760c5379
m-ball3 <mball3@uw.edu>
2025-12-08 15:33:35 -0800

    saves excel table

--- Scripts/write to excel.R
+++ Scripts/write to excel.R
@@ -1,14 +1,14 @@
-install.packages("writexl")
+#install.packages("writexl")
 library(writexl)
 
 ls()
 class("taxa")
 
-df <- as.data.frame(merged.taxa)
+df <- as.data.frame(taxa)
 df_with_rownames <- cbind(rownames = rownames(df), df)
-writexl::write_xlsx(df_with_rownames, "WADE003-arcticpred_dada2_QAQC_16SP1+2.xlsx")
+writexl::write_xlsx(df_with_rownames, "./Deliverables/16S/WADE003-arcticpred_dada2_QAQC_16SP1+2.xlsx")
 
 
-write_xlsx(df, "mWADE003-arcticpred-16SP2-taxa.xlsx")
+write_xlsx(df, "/Deliverables/16S/mWADE003-arcticpred-16SP2-taxa.xlsx")
 
 getwd()
